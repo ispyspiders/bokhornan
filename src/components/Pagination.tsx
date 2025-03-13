d01c75c6befb0fa140b66e9d913d2b47e01b4889
Kajsa Classon <ispyspiders@gmail.com>
2025-03-13 13:52:49 +0100

    uppdaterat paginering för att återspegla antal sökresultat och vilka som visas av dessa

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -10,14 +10,19 @@ interface PaginationProps {
 }
 
 const Pagination: React.FC<PaginationProps> = ({ startIndex, totalItems, maxResults, onPrevious, onNext }) => {
+
+    // Beräkna intervallet som visas för nuvarande sida
+    const from = startIndex +1;
+    const to = Math.min(startIndex+maxResults, totalItems); // visa max "totalitems" på sista sidan
+
     return (
-        <div className='w-full mt-4 bg-light flex items-center justify-between rounded-lg text-sm gap-4'>
+        <div className='w-full mt-4 bg-light flex items-center justify-between rounded-lg text-sm gap-4 mb-2'>
             <button onClick={onPrevious} disabled={startIndex === 0} className='flex rounded-s-lg py-2 m-0 ps-4 pe-8 border border-e-0 border-blush-mid drop-shadow-sm bg-blush-deep text-sm'>
                 <CaretLeft size={24} className='' />
                 
             </button>
             <span>
-                Visar resultat {startIndex} - {startIndex + maxResults} av {totalItems}
+                Visar resultat {from} - {to} av {totalItems}
             </span>
             <button onClick={onNext} disabled={startIndex + maxResults >= totalItems} className='flex rounded-e-lg py-2 m-0 ps-4 pe-8 border border-s-0 border-blush-mid drop-shadow-sm bg-blush-deep text-sm'>
                 <CaretRight size={24} />
