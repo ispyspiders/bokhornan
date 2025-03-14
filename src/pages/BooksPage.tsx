77a86fbc718874da6285f0569f0476e08e686ba4
Kajsa Classon <ispyspiders@gmail.com>
2025-03-14 09:23:54 +0100

    Uppdaterat sök info.

--- src/pages/BooksPage.tsx
+++ src/pages/BooksPage.tsx
@@ -33,18 +33,21 @@ const BooksPage = () => {
             <h2 className='mb-2'>Böcker</h2>
             <p className='text-lg text-dark-soft'>Här kan du söka efter böcker baserat på titel, författare eller genre, samt upptäcka de mest populära böckerna just nu. Utforska böcker för att hitta din nästa läsupplevelse!</p>
 
-            <SearchForm searchTerm={searchTerm} onSearchChange={handleSearchChange} error={error} onSubmit={handleSubmit}/>
+            <SearchForm searchTerm={searchTerm} onSearchChange={handleSearchChange} error={error} onSubmit={handleSubmit} />
 
             <h3 >Sök efter böcker</h3>
             <p>Använd vårt sökverktyg för att snabbt hitta böcker som passar din smak. Du kan söka på:</p>
             <ul className='font-serif list-disc ms-12'>
-                <li className='mb-2'><span className='font-bold'>Titel</span> <br /> Skriv bokens namn för att hitta den direkt.</li>
-                <li className='mb-2'><span className='font-bold'>Författare</span> <br /> Skriv författarens namn för att hitta deras verk.</li>
-                <li className='mb-2'><span className='font-bold'>Genre</span> <br /> Filtrera böcker efter genre, till exempel fantasy, thriller, romance, och mycket mer.</li>
+                <li className='mb-2'><span className='font-bold'>Titel</span> <br />Skriv <em>intitle:</em> följt av sökord för att endast söka inom titel. <br /> Exempel: <span className='font-sans ms-2'>intitle:Collecting cats</span></li>
+                <li className='mb-2'><span className='font-bold'>Författare</span> <br /> Skriv <em>inauthor:</em> följt av sökord för att söka inom författare. <br /> Exempel: <span className='font-sans ms-2'>inauthor:Lorna Scobie</span></li>
+                <li className='mb-2'><span className='font-bold'>Genre</span> <br /> Skriv <em>subject:</em> följt av sökord för att söka inom genre. Exempel: <span className='font-sans ms-2'>subject:Juvenile Fiction</span></li>
             </ul>
+            <div className="bg-light rounded-lg p-4 mt-8 drop-shadow-sm w-fit mx-8">
+                <p className='max-w-none m-0'>Tips! För att få mer exakta resultat skriv din sökterm inom citattecken. <br />Exempel: <span className='font-sans ms-2'>"Lorna Scobie"</span> istället för <span className='font-sans'>Lorna Scobie</span>.</p>
+            </div>
 
             <section className='mt-12'>
-                <h2>Topplista - De 4 mest gillade böckerna</h2>
+                <h2>Topplista - De 5 mest gillade böckerna</h2>
                 <p className='text-lg text-dark-soft'>Här är de 5 böcker som just nu får mest kärlek från våra användare! Håll dig uppdaterad med de senaste favoriterna och hitta inspiration för din nästa bok.</p>
                 <div className='bg-wave'>
                     läs ut mest gillade böcker
