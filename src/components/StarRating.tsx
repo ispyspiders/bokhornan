cba39f84fb3f601c1bc533229a789b3863346060
Kajsa Classon <ispyspiders@gmail.com>
2025-03-15 09:24:36 +0100

    Lagt till size och readonly för starrating

--- src/components/StarRating.tsx
+++ src/components/StarRating.tsx
@@ -2,23 +2,46 @@ import { Star } from "@phosphor-icons/react";
 import { useState } from "react";
 
 interface StarRatingProps {
-    onRatingChange: (rating: number) => void,
-    rating: number
+    onRatingChange?: (rating: number) => void,
+    rating: number,
+    readonly?: boolean,
+    size: number
 }
 
-const StarRating: React.FC<StarRatingProps> = ({ onRatingChange, rating }) => {
-    // const [rating, setRating] = useState(0); // betyg (1-5)
+const StarRating: React.FC<StarRatingProps> = ({ onRatingChange, rating, readonly, size }) => {
+    const [hovered, setHovered] = useState<number | null>(null);
+
+    const handleMouseEnter = (index: number) => {
+        if (!readonly) {
+            setHovered(index);
+        }
+    }
+
+    const handleMouseLeave = () => {
+        if (!readonly) {
+            setHovered(null);
+        }
+    }
 
     const handleClick = (newRating: number) => {
-        // setRating(newRating);
-        onRatingChange(newRating); // Skicka betyg till förälder
+        if (!readonly && onRatingChange) {
+            onRatingChange(newRating); // Skicka betyg till förälder
+        }
     };
 
     const renderStars = () => {
         const stars = [];
         for (let i = 1; i <= 5; i++) {
             stars.push(
-                <Star weight="fill" size={24} key={i} onClick={() => handleClick(i)} className={`cursor-pointer me-2 ${i <= rating ? 'text-coral' : 'text-blush-mid'}`} />
+                <Star 
+                    weight="fill" 
+                    size={size} 
+                    key={i} 
+                    onClick={() => handleClick(i)} 
+                    onMouseEnter={()=> handleMouseEnter(i+1)}
+                    onMouseLeave={handleMouseLeave}
+                    className={`${readonly ? 'cursor-default':'cursor-pointer'} me-2 ${i <= rating ? 'text-coral' : 'text-blush-mid'}`} 
+                    />
             );
         }
         return stars;
