f40debf9afa32fb9b8330e98f4bae4ce509f7d37
Kajsa Classon <ispyspiders@gmail.com>
2025-03-17 12:18:19 +0100

    Innehåll till om oss

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,7 +1,47 @@
 
 const AboutPage = () => {
   return (
-    <div>AboutPage</div>
+    <div className='p-4 py-12 md:p-12'>
+      <h2>Om oss</h2>
+      <p className="font-display text-dark-soft text-4xl mx-auto text-center ">Välkommen till Bokhörnan <br /><span className="text-2xl"> Din bokälskande plats på nätet! </span>
+      </p>
+      <p className="mx-auto text-lg my-8 text-dark-soft">Bokhörnan är din digitala bokhylla där du kan upptäcka, läsa och dela dina bokupplevelser. Vi är ett community för alla bokälskare – oavsett om du letar efter nästa läsupplevelse eller vill dela dina tankar om en bok med andra.
+      </p>
+
+      <div className="w-fit mx-auto">
+        <h3>Vad vi gör</h3>
+        <p>På Bokhörnan gör vi det enkelt att hitta böcker som passar just dig. Med vår sökfunktion, som är integrerad med Google Books API, kan du snabbt hitta böcker från hela världen – oavsett genre. Här hittar du allt från de senaste bästsäljarna till gamla klassiker och nya författare.</p>
+        <p>Men Bokhörnan är inte bara en plats för att söka böcker – det är också en plattform för att läsa och skriva recensioner. Vill du veta vad andra tycker om en bok innan du slår till? Eller vill du dela din egen åsikt? Här på Bokhörnan kan du läsa recensioner från andra användare, och om du har ett konto kan du även skriva egna recensioner och betygsätta böcker.</p>
+
+        <h3>Gilla böcker</h3>
+        <p>Förutom att läsa och skriva recensioner kan du också "gilla" böcker – på så sätt kan du skapa en personlig samling av favoriter och inspirera och insprireras av andra användares boksamlingar.</p>
+
+        <h3>En plats för alla bokälskare</h3>
+        <p>Oavsett om du är en passionerad bokslukare eller en som gillar att läsa sporadiskt, vill vi på Bokhörnan vara din go-to-plats för alla saker som har med böcker att göra. Vårt mål är att skapa en gemenskap där alla kan dela sin kärlek till litteraturen och hitta nya böcker att älska.</p>
+
+        <h3>Varför Bokhörnan?</h3>
+        <ul>
+          <li>
+            <p>
+              Enkel boksearch: Sök efter böcker genom vårt smidiga gränssnitt och hitta allt från de senaste titlarna till dolda pärlor.
+            </p>
+          </li>
+          <li>
+            <p>
+              Recensioner och betyg: Läs andras åsikter om böcker och dela din egen recension när du har läst klart.
+            </p>
+          </li>
+          <li>
+            <p>
+              Logga in och interagera: Skapa ett konto för att skriva recensioner, gilla böcker och få ännu mer personaliserade rekommendationer.
+            </p>
+          </li>
+        </ul>
+        <h3>Bli en del av Bokhörnan
+        </h3>
+        <p>Oavsett om du vill hitta din nästa bok, skriva en recension eller helt enkelt dela din passion för litteratur, så är Bokhörnan här för dig. Skapa ett konto idag och bli en del av vårt växande community av bokälskare!</p>
+      </div>
+    </div>
   )
 }
 
